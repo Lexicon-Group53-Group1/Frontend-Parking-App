1f3fea6817c26c48ba41d64208523eae979eb346
fatima bouchtaoui <amitaoui@yahoo.com>
2025-03-14 13:48:42 +0100

    Implement Login Component

--- frontend-parking-app/src/pages/Login.jsx
+++ frontend-parking-app/src/pages/Login.jsx
@@ -1,24 +1,57 @@
-import {useForm} from 'react-hook-form';
-import "../styles/style.css";
+import { useForm } from 'react-hook-form';
+import '../styles/style.css';
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+
 
 export const Login = () => {
-    const{register, handleSubmit} =useForm();
-    const onSubmit = (data) => { console.log(data);}
+    const{handleSubmit} = useForm();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const onSubmit = async () => {
+        const response = await fetch("http://localhost:5244/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ username, password }),
+        });
+        
+        if(response.ok){
+            const data = await response.json();
+            localStorage.setItem("user", JSON.stringify(data.user));
+            alert("Successful login!")
+            navigate("/session");
+        } else {
+            alert("Failed to login. Check your credentials.")   
+        }
+         
+    }
     return (
         <div className="container">
             <div className="form-box">
                 <h2>Login</h2>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="input-group">
-                        <label>User name:</label>
-                        <input type="email" placeholder="Enter your Email" {...register('email')} required/>
+                        <label>Username:</label>
+                        <input 
+                            type="username" 
+                            placeholder="Enter your username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
                     </div>
                     <div className="input-group">
                         <label>Password:</label>
-                        <input type="password" placeholder="Enter Password" {...register('password')} required/>
+                        <input 
+                            type="password" 
+                            placeholder="Enter your password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
                     </div>
                     <div className="btn-group">
-                        <button type="submit">Login</button>
+                        <button type="submit" onClick={onSubmit}>Login</button>
                     </div>
                 </form>
             </div>
